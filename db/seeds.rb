0bc80a60f26c4597f485a93567fcef3ed31f6b12
tarekdinaji <mtarekhossain@gmail.com>
2023-10-08 21:58:34 +0600

    new category bootstrap configured

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+#Category.create(name: "Men", details: "Men's wear main category")
+#Category.create(name: "Women", details: "Women's wear main category")
+Category.create(name: "Shirt", category_id: 1, details: "Shirt category for Men")
+Category.create(name: "Hijab", category_id: 2, details: "Hijab category for Women")
+
+#Product.create(name: "Polo_shirt_1", description: "Mens wear", price: 40, category_id: 3)
+#Product.create(name: "T-shirt_1", description: "Mens wear", price: 25, category_id: 3)
+#Product.create(name: "Gree_44", description: "sgsdgg", price: 90, category_id: 4)
+#Product.create(name: "Voo_45", description: "uhshgs", price: 120, category_id: 4)
+
+#Varient.create(color: "red", size: "44", sku: "773450", product_id: 1)
+#Varient.create(color: "green", size: "38", sku: "695470", product_id: 2)
+#Varient.create(color: "yellow", size: "28", sku: "290704", product_id: 3)
+#Varient.create(color: "white", size: "24", sku: "1095385", product_id: 4)
+
+#User.create(first_name: "Bob", last_name: "Marley", email:"bob@gmail.com", password: "1234")
